8658a90588fb4b0e683c306d9b7655ddcce82807
G'lek Tarssza <glek@glektarssza.com>
2024-02-20 09:24:32 -0700

    Finished up utils tests.

--- tests/v2/utils.spec.ts
+++ tests/v2/utils.spec.ts
@@ -42,7 +42,7 @@ describe('module:utils', () => {
             const r = testModule.getGitHubActionStage();
 
             //-- Then
-            expect(r).toEqual(GitHubActionStage.PostMain);
+            expect(r).toBe(GitHubActionStage.PostMain);
         });
         it('should return `GitHubActionStage.PostMain` if the `mainFinished` state is set', () => {
             //-- Given
@@ -56,7 +56,7 @@ describe('module:utils', () => {
             const r = testModule.getGitHubActionStage();
 
             //-- Then
-            expect(r).toEqual(GitHubActionStage.PostMain);
+            expect(r).toBe(GitHubActionStage.PostMain);
         });
         it('should return `GitHubActionStage.Main` if the `mainStarted` state is set', () => {
             //-- Given
@@ -70,7 +70,7 @@ describe('module:utils', () => {
             const r = testModule.getGitHubActionStage();
 
             //-- Then
-            expect(r).toEqual(GitHubActionStage.Main);
+            expect(r).toBe(GitHubActionStage.Main);
         });
         it('should return `GitHubActionStage.Main` if the `preMainFinished` state is set', () => {
             //-- Given
@@ -84,7 +84,7 @@ describe('module:utils', () => {
             const r = testModule.getGitHubActionStage();
 
             //-- Then
-            expect(r).toEqual(GitHubActionStage.Main);
+            expect(r).toBe(GitHubActionStage.Main);
         });
         it('should return `GitHubActionStage.PreMain` if no state is set', () => {
             //-- Given
@@ -98,7 +98,178 @@ describe('module:utils', () => {
             const r = testModule.getGitHubActionStage();
 
             //-- Then
-            expect(r).toEqual(GitHubActionStage.PreMain);
+            expect(r).toBe(GitHubActionStage.PreMain);
+        });
+    });
+    describe('.isGitHubActionStage()', () => {
+        let mockedGetStage: jest.SpyInstance<
+            ReturnType<typeof testModule.getGitHubActionStage>
+        >;
+        beforeAll(() => {
+            mockedGetStage = jest.spyOn(testModule, 'getGitHubActionStage');
+        });
+        afterAll(() => {
+            mockedGetStage.mockRestore();
+        });
+        it('should return `true` if passed `GitHubActionStage.PreMain` and the stage is pre-main', () => {
+            //-- Given
+            mockedGetStage.mockReturnValueOnce(GitHubActionStage.PreMain);
+
+            //-- When
+            const r = testModule.isGitHubActionStage(GitHubActionStage.PreMain);
+
+            //-- Then
+            expect(r).toBe(true);
+        });
+        it('should return `false` if passed `GitHubActionStage.PreMain` and the stage is not pre-main', () => {
+            //-- Given
+            mockedGetStage.mockReturnValueOnce(GitHubActionStage.Main);
+
+            //-- When
+            const r = testModule.isGitHubActionStage(GitHubActionStage.PreMain);
+
+            //-- Then
+            expect(r).toBe(false);
+        });
+        it('should return `true` if passed `GitHubActionStage.Main` and the stage is main', () => {
+            //-- Given
+            mockedGetStage.mockReturnValueOnce(GitHubActionStage.Main);
+
+            //-- When
+            const r = testModule.isGitHubActionStage(GitHubActionStage.Main);
+
+            //-- Then
+            expect(r).toBe(true);
+        });
+        it('should return `false` if passed `GitHubActionStage.Main` and the stage is not main', () => {
+            //-- Given
+            mockedGetStage.mockReturnValueOnce(GitHubActionStage.PreMain);
+
+            //-- When
+            const r = testModule.isGitHubActionStage(GitHubActionStage.Main);
+
+            //-- Then
+            expect(r).toBe(false);
+        });
+        it('should return `true` if passed `GitHubActionStage.PostMain` and the stage is post-main', () => {
+            //-- Given
+            mockedGetStage.mockReturnValueOnce(GitHubActionStage.PostMain);
+
+            //-- When
+            const r = testModule.isGitHubActionStage(
+                GitHubActionStage.PostMain
+            );
+
+            //-- Then
+            expect(r).toBe(true);
+        });
+        it('should return `false` if passed `GitHubActionStage.PostMain` and the stage is not post-main', () => {
+            //-- Given
+            mockedGetStage.mockReturnValueOnce(GitHubActionStage.Main);
+
+            //-- When
+            const r = testModule.isGitHubActionStage(
+                GitHubActionStage.PostMain
+            );
+
+            //-- Then
+            expect(r).toBe(false);
+        });
+    });
+    describe('.isPreMainGitHubActionStage()', () => {
+        let mockedIsStage: jest.SpyInstance<
+            ReturnType<typeof testModule.isGitHubActionStage>,
+            Parameters<typeof testModule.isGitHubActionStage>
+        >;
+        beforeAll(() => {
+            mockedIsStage = jest.spyOn(testModule, 'isGitHubActionStage');
+        });
+        afterAll(() => {
+            mockedIsStage.mockRestore();
+        });
+        it('should return `true` if the stage is pre-main', () => {
+            //-- Given
+            mockedIsStage.mockReturnValueOnce(true);
+
+            //-- When
+            const r = testModule.isPreMainGitHubActionStage();
+
+            //-- Then
+            expect(r).toBe(true);
+        });
+        it('should return `false` if the stage is not pre-main', () => {
+            //-- Given
+            mockedIsStage.mockReturnValueOnce(false);
+
+            //-- When
+            const r = testModule.isPreMainGitHubActionStage();
+
+            //-- Then
+            expect(r).toBe(false);
+        });
+    });
+    describe('.isMainGitHubActionStage()', () => {
+        let mockedIsStage: jest.SpyInstance<
+            ReturnType<typeof testModule.isGitHubActionStage>,
+            Parameters<typeof testModule.isGitHubActionStage>
+        >;
+        beforeAll(() => {
+            mockedIsStage = jest.spyOn(testModule, 'isGitHubActionStage');
+        });
+        afterAll(() => {
+            mockedIsStage.mockRestore();
+        });
+        it('should return `true` if the stage is main', () => {
+            //-- Given
+            mockedIsStage.mockReturnValueOnce(true);
+
+            //-- When
+            const r = testModule.isMainGitHubActionStage();
+
+            //-- Then
+            expect(r).toBe(true);
+        });
+        it('should return `false` if the stage is not main', () => {
+            //-- Given
+            mockedIsStage.mockReturnValueOnce(false);
+
+            //-- When
+            const r = testModule.isMainGitHubActionStage();
+
+            //-- Then
+            expect(r).toBe(false);
+        });
+    });
+    describe('.isPostMainGitHubActionStage()', () => {
+        let mockedIsStage: jest.SpyInstance<
+            ReturnType<typeof testModule.isGitHubActionStage>,
+            Parameters<typeof testModule.isGitHubActionStage>
+        >;
+        beforeAll(() => {
+            mockedIsStage = jest.spyOn(testModule, 'isGitHubActionStage');
+        });
+        afterAll(() => {
+            mockedIsStage.mockRestore();
+        });
+        it('should return `true` if the stage is post-main', () => {
+            //-- Given
+            mockedIsStage.mockReturnValueOnce(true);
+
+            //-- When
+            const r = testModule.isPostMainGitHubActionStage();
+
+            //-- Then
+            expect(r).toBe(true);
+        });
+        it('should return `false` if the stage is not post-main', () => {
+            //-- Given
+            mockedIsStage.mockReturnValueOnce(false);
+
+            //-- When
+            const r = testModule.isPostMainGitHubActionStage();
+
+            //-- Then
+            expect(r).toBe(false);
         });
     });
 });
